7b71ec906c46035b6c46b2598ad19cc85b0a2ba6
agorapilled <johngardner13@outlook.com>
2024-12-23 03:59:58 -0500

    v0

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
   return (
     <Html lang="ja">
       <Head />
-      <body style={{ backgroundImage: `url(${buildUrl("/b.gif")})` }}>
+      <body style={{ backgroundImage: `url(${buildUrl("/bg-c.png")})` }}>
         <Main />
         <NextScript />
       </body>
