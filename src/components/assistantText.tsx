8adbaacc5b4a5685c63eeb0db167ef7147382268
agorapilled <johngardner13@outlook.com>
2024-12-23 03:45:43 -0500

    v0

--- src/components/assistantText.tsx
+++ src/components/assistantText.tsx
@@ -3,10 +3,10 @@ export const AssistantText = ({ message }: { message: string }) => {
     <div className="absolute bottom-0 left-0 mb-[60px] w-full">
       <div className="mx-auto max-w-4xl w-full p-16">
         <div className="bg-white rounded-8">
-          <div className="px-24 py-8 bg-black rounded-8 text-white font-Montserrat font-bold tracking-wider">
+          <div className="px-24 py-8 bg-black rounded-t-8 text-white font-Montserrat font-bold tracking-wider">
             CHARACTER
           </div>
-          <div className="px-24 py-16 bg-black">
+          <div className="px-24 py-16 bg-black rounded-b-8">
             <div className="text-white typography-16 font-M_PLUS_2 font-bold">
               {message.replace(/\[([a-zA-Z]*?)\]/g, "")}
             </div>
