265b4d79f139fbd97ad957578a38716415409726
agorapilled <johngardner13@outlook.com>
2024-12-23 03:22:49 -0500

    v0

--- src/components/assistantText.tsx
+++ src/components/assistantText.tsx
@@ -1,6 +1,6 @@
 export const AssistantText = ({ message }: { message: string }) => {
   return (
-    <div className="absolute bottom-0 left-0 mb-104 w-full hidden">
+    <div className="absolute bottom-0 left-0 mb-104 w-full">
       <div className="mx-auto max-w-4xl w-full p-16">
         <div className="bg-white rounded-8">
           <div className="px-24 py-8 bg-black rounded-t-8 text-white font-Montserrat font-bold tracking-wider">
