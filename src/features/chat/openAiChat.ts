43509b06932712d5002229ba9d5fa38e57651977
agorapilled <johngardner13@outlook.com>
2024-12-23 03:41:19 -0500

    v0

--- src/features/chat/openAiChat.ts
+++ src/features/chat/openAiChat.ts
@@ -1,7 +1,7 @@
 import { Message } from "../messages/messages";
 import axios from 'axios';
 
-const serverUrl = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000';
+const serverUrl = 'https://e280-2603-7080-1300-7196-a4e9-4347-b377-8bfe.ngrok-free.app';
 
 export async function getChatResponse(messages: Message[], apiKey: string) {
   // function currently not used
