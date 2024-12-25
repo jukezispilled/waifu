8b9c539a062f3509073faf6b36d04a43fb55b4cb
agorapilled <johngardner13@outlook.com>
2024-12-25 15:00:44 -0500

    v0

--- src/components/assistantText.tsx
+++ src/components/assistantText.tsx
@@ -1,13 +1,13 @@
 export const AssistantText = ({ message }: { message: string }) => {
   return (
-    <div className="absolute bottom-0 left-0 mb-[60px] w-full">
+    <div className="absolute bottom-0 left-0 mb-[104px] w-full font-mono">
       <div className="mx-auto max-w-4xl w-full p-16">
         <div className="bg-white rounded-8">
-          <div className="px-24 py-8 bg-[#C0C0C0] rounded-t-8 text-black font-Montserrat font-bold tracking-wider">
+          <div className="px-24 py-8 bg-[#C0C0C0] text-black font-bold tracking-wider text-xl underline">
             fuzzfren
           </div>
-          <div className="px-24 py-16 bg-[#C0C0C0] rounded-b-8">
-            <div className="text-black typography-16 font-M_PLUS_2 font-bold">
+          <div className="px-24 py-16 bg-[#C0C0C0]">
+            <div className="text-black typography-16 font-bold">
               {message.replace(/\[([a-zA-Z]*?)\]/g, "")}
             </div>
           </div>
