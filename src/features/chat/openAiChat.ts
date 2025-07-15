aa3ac7bf46b4fdd1249bca3b1ce812af42f4f048
agorapilled <johngardner13@outlook.com>
2025-07-14 22:11:57 -0400

    v0

--- src/features/chat/openAiChat.ts
+++ src/features/chat/openAiChat.ts
@@ -70,10 +70,8 @@ export async function getChatResponseStream(
             // "model": "openai/gpt-3.5-turbo",
             // "model": "cohere/command-r-plus",
             // "model": "anthropic/claude-3.5-sonnet:beta",
-            "model": "x-ai/grok-3-mini",
+            "model": "x-ai/grok-4",
             "messages": messages,
-            "temperature": 0.7,
-            "max_tokens": 200,
             "stream": true,
           })
         });
