2e301ca521812701188b65c81ad1d6cde3a0408d
agorapilled <johngardner13@outlook.com>
2025-07-14 21:48:38 -0400

    v0

--- src/features/chat/openAiChat.ts
+++ src/features/chat/openAiChat.ts
@@ -70,7 +70,7 @@ export async function getChatResponseStream(
             // "model": "openai/gpt-3.5-turbo",
             // "model": "cohere/command-r-plus",
             // "model": "anthropic/claude-3.5-sonnet:beta",
-            "model": "microsoft/wizardlm-2-8x22b",
+            "model": "x-ai/grok-3",
             "messages": messages,
             "temperature": 0.7,
             "max_tokens": 200,
